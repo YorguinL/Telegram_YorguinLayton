4de16cba1d3efdd8353d154c71c367152b2c23bc
YorguinL <56040482+YorguinL@users.noreply.github.com>
2019-11-19 19:59:35 +0100

    Update programaBot.py
    V2

--- Bot/programaBot.py
+++ Bot/programaBot.py
@@ -30,3 +30,44 @@ class ProgramaBot:
 if __name__ == '__main__':
     nums = ProgramaBot(sys.argv[1])
     print nums.comprovar()
+
+    
+import random
+import sys
+
+class ProgramaBot:
+
+    def __init__(self, numUsuari):
+        self.numUsuari = int(input("Introdueix un nmero entre 0 i 100: "))
+        self.count = 0
+        nums.comprovar()
+
+    def comprovar(self, numUsuari):
+
+        if (numUsuari >= 1 and numUsuari <= 100):
+            self.nAleatori = random.randint(0,100)
+            numTrobat = False
+            self.comprovar()
+            while not numTrobat:
+                for i in range(15):
+                    if numUsuari != numAleatori:
+                        count = i
+                        numTrobat == False
+                        print("Nmero incorrecte")
+                        self.numUsuari = int(input(""))
+                    else:
+                        numTrobat == True
+                        print("Nmero correcte")
+                        print("L'has endevinat en " + str(count) + " intents.")
+        else :
+            print("El valor es troba fora del rang.")
+
+if __name__ == '__main__':
+    nums = ProgramaBot(sys.argv[1])
+    print nums.comprovar()
+
+    
+    
+    
+    
+    

cb5036a730a86b72ce2ad4cccb86d408fdd5f984
YorguinL <56040482+YorguinL@users.noreply.github.com>
2019-11-28 19:23:14 +0100

    Add files via upload

--- Bot/NumsBot.py
+++ Bot/NumsBot.py
@@ -2,20 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import telebot
+from programaBot import ProgramaBot
 
 TOKEN = '940620192:AAG5K2iDKH_ATTxWmz8iz2a3Ppi-v4srAF8'
 bot = telebot.TeleBot(TOKEN)
+p = ProgramaBot()
+p.idUsuari
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Heeeey!")
-	bot.send_message('673273081', 'hdp')
+@bot.message_handler(commands=['inici'])
+def mensaje_bienvenida(message):
+	bot.reply_to(message, "Heeeeey! Para jugar utiliza el comando /jugar y si tienes dudas utiliza /ayuda")
+	p.idUsuari = message.user.id
+@bot.message_handler(commands=['jugar'])
+def comenzar_juego(message):
+	bot.reply_to(message, "Comencemos! Introduce un número del 0 al 100.")
 
-@bot.message_handler ( func = lambda  m : True )
-def  echo_all(message):
-    print(message.chat.id)
-    bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda message: True)
+def comenzar_juego(message):
+	try:
+		p.numUsuari=(int(message.text))
+		p.comprovar()
+		if p.rangValid == True:
+			if p.solucio == True:
+				bot.send_message('673273081', "L'has endevinat en " + str(p.count) + " intents.")
+			else:
+				bot.send_message('673273081', "Nmero incorrecte, torna-ho a intentar")
+		else:
+			bot.send_message('673273081', "Estas fora del rang")
+		print message.user.id
+		print p.idUsuari
+		print p.numUsuari
+		print p.nAleatori
+	except:
+		pass
 
-bot.send_message('673273081', 'hdp')
-
+#bot.send_message('673273081', 'hdp')
 bot.polling()
--- Bot/programaBot.py
+++ Bot/programaBot.py
@@ -1,73 +1,33 @@
+
 import random
 import sys
 
 class ProgramaBot:
 
-    def __init__(self, numUsuari):
-        self.numUsuari = int(input("En quin nsmero estic pensant?"))
+    def __init__(self):
+        self.idUsuari = ""
+        self.numUsuari = 0
         self.count = 0
-        nums.comprovar()
+        self.nAleatori = random.randint(1,101)
+        self.rangValid = False
+        self.solucio = False
 
-    def comprovar(self, numUsuari):
 
-        if (numUsuari >= 1 and numUsuari <= 100):
-            self.nAleatori = random.randint(1,100)
-            numTrobat = False
-            self.comprovar()
-            while not numTrobat:
-                for i in range(15):
-                    if numUsuari != numAleatori:
-                        count = i
-                        numTrobat == False
-                        print("Nmero incorrecte")
-                        self.numUsuari = int(input("En quin nmero estic pensant?"))
-                    else:
-                        numTrobat == True
-                        print("Nmero correcte")
-        else :
-            print("Valor incorrecte")
-
-if __name__ == '__main__':
-    nums = ProgramaBot(sys.argv[1])
-    print nums.comprovar()
-
-    
-import random
-import sys
-
-class ProgramaBot:
-
-    def __init__(self, numUsuari):
-        self.numUsuari = int(input("Introdueix un nmero entre 0 i 100: "))
-        self.count = 0
-        nums.comprovar()
-
-    def comprovar(self, numUsuari):
-
-        if (numUsuari >= 1 and numUsuari <= 100):
-            self.nAleatori = random.randint(0,100)
-            numTrobat = False
-            self.comprovar()
-            while not numTrobat:
-                for i in range(15):
-                    if numUsuari != numAleatori:
-                        count = i
-                        numTrobat == False
-                        print("Nmero incorrecte")
-                        self.numUsuari = int(input(""))
-                    else:
-                        numTrobat == True
-                        print("Nmero correcte")
-                        print("L'has endevinat en " + str(count) + " intents.")
-        else :
-            print("El valor es troba fora del rang.")
-
-if __name__ == '__main__':
-    nums = ProgramaBot(sys.argv[1])
-    print nums.comprovar()
-
-    
-    
-    
-    
-    
+    def comprovar(self):
+        numTrobat = False
+        if self.numUsuari in range(101):
+            self.count +=1
+            self.rangValid = True
+            if not numTrobat:
+                if self.numUsuari != self.nAleatori:
+                    numTrobat == False
+                    print("Nmero incorrecte, torna-ho a intentar")
+                    self.solucio = False
+                else:
+                    numTrobat == True
+                    print("Nmero correcte")
+                    self.solucio = True
+                    print("L'has endevinat en " + str(self.count) + " intents.")
+        else:
+            self.rangValid = False
+            print("Estas fora del rang")
